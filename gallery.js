7344ba437dfd45283ca4ed1794734c5c76ad4088
Deepti0604 <150773402+Deepti0604@users.noreply.github.com>
2024-08-31 20:08:42 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,48 @@
+function naturePic() {
+    document.getElementById("nature_images_section").style.display = "block";
+    document.getElementById("valcano_images_section").style.display = "none";
+    document.getElementById("landscape_images_section").style.display = "none";
+    document.getElementById("ocean_images_section").style.display = "none";
+    document.getElementById("wildlife_images_section").style.display = "none";
+    document.getElementById("mountain_images_section").style.display = "none";
+}
+function valcanoPic() {
+    document.getElementById("nature_images_section").style.display = "none";
+    document.getElementById("valcano_images_section").style.display = "block";
+    document.getElementById("landscape_images_section").style.display = "none";
+    document.getElementById("ocean_images_section").style.display = "none";
+    document.getElementById("wildlife_images_section").style.display = "none";
+    document.getElementById("mountain_images_section").style.display = "none";
+}
+function landscapePic() {
+    document.getElementById("nature_images_section").style.display = "none";
+    document.getElementById("valcano_images_section").style.display = "none";
+    document.getElementById("landscape_images_section").style.display = "block";
+    document.getElementById("ocean_images_section").style.display = "none";
+    document.getElementById("wildlife_images_section").style.display = "none";
+    document.getElementById("mountain_images_section").style.display = "none";
+}
+function oceanPic() {
+    document.getElementById("nature_images_section").style.display = "none";
+    document.getElementById("valcano_images_section").style.display = "none";
+    document.getElementById("landscape_images_section").style.display = "none";
+    document.getElementById("ocean_images_section").style.display = "block";
+    document.getElementById("wildlife_images_section").style.display = "none";
+    document.getElementById("mountain_images_section").style.display = "none";
+}
+function wildlifePic() {
+    document.getElementById("nature_images_section").style.display = "none";
+    document.getElementById("valcano_images_section").style.display = "none";
+    document.getElementById("landscape_images_section").style.display = "none";
+    document.getElementById("ocean_images_section").style.display = "none";
+    document.getElementById("wildlife_images_section").style.display = "block";
+    document.getElementById("mountain_images_section").style.display = "none";
+}
+function mountainPic() {
+    document.getElementById("nature_images_section").style.display = "none";
+    document.getElementById("valcano_images_section").style.display = "none";
+    document.getElementById("landscape_images_section").style.display = "none";
+    document.getElementById("ocean_images_section").style.display = "none";
+    document.getElementById("wildlife_images_section").style.display = "none";
+    document.getElementById("mountain_images_section").style.display = "block";
+}
